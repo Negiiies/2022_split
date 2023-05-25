a3016ad692a58f57c3b57d481b43063c17d8efa7
LE Dylan <dylan.le@ecole-89.com>
2023-05-25 16:04:20 +0200

    Sauvegarde

--- src/split.c
+++ src/split.c
@@ -28,7 +28,6 @@ static int count_letter(const char *str, char token, int index)
 {
     int counter;
 
-    index += 1;
     counter = 0;
     while (str[index] != '\0' && str[index] != token) {
         index += 1;
@@ -37,29 +36,21 @@ static int count_letter(const char *str, char token, int index)
     return counter;
 }
 
-char **split(const char *str, char token)
+static void while_function(const char *str, char **sp, char token, int j)
 {
     int i;
-    int j;
     int k;
-    char **sp;
 
     i = 0;
-    j = 0;
     k = 0;
-    if (!str) {
-        return NULL;
-    }
-    sp = malloc(sizeof(char *) * (token_counter(str, token) + 2));
-    sp[token_counter(str, token) + 1] = NULL;
     while (str[i] != '\0') {
         if (str[i] == token
             || (i == 0 && str[i] != token)) {
-            sp[j] = malloc(sizeof(char) * (count_letter(str, token, i) + 1));
-            sp[j][count_letter(str, token, i)] = '\0';
             if (str[i] == token) {
                 i += 1;
             }
+            sp[j] = malloc(sizeof(char) * (count_letter(str, token, i) + 1));
+            sp[j][count_letter(str, token, i)] = '\0';
             while (str[i] != token && str[i] != '\0') {
                 sp[j][k] = str[i];
                 i += 1;
@@ -71,5 +62,24 @@ char **split(const char *str, char token)
             i += 1;
         }
     }
+}
+
+char **split(const char *str, char token)
+{
+    int j;
+    char **sp;
+
+    j = 0;
+    if (!str) {
+        return NULL;
+    }
+    sp = malloc(sizeof(char *) * (token_counter(str, token) + 2));
+    sp[token_counter(str, token) + 1] = NULL;
+    if (str[0] == token) {
+        sp[0] = malloc(0);
+        sp[0][0] = '\0';
+        j += 1;
+    }
+    while_function(str, sp, token, j);
     return sp;
 }
--- test/split.c
+++ test/split.c
@@ -19,3 +19,40 @@ Test(split, normal)
     cr_assert_str_eq(sp[1], "world");
     cr_assert_not(sp[2]);
 }
+
+Test(split, token_at_the_end)
+{
+    char **sp;
+
+    sp = split("hello;world;", ';');
+    cr_assert(sp);
+    cr_assert_str_eq(sp[0], "hello");
+    cr_assert_str_eq(sp[1], "world");
+    cr_assert_str_eq(sp[2], "");
+    cr_assert_not(sp[3]);
+}
+
+Test(split, token_at_the_edges)
+{
+    char **sp;
+
+    sp = split(";hello;world;", ';');
+    cr_assert(sp);
+    cr_assert_str_eq(sp[0], "");
+    cr_assert_str_eq(sp[1], "hello");
+    cr_assert_str_eq(sp[2], "world");
+    cr_assert_str_eq(sp[3], "");
+    cr_assert_not(sp[4]);
+}
+
+Test(split, token_at_the_beginning)
+{
+    char **sp;
+
+    sp = split(";hello;world", ';');
+    cr_assert(sp);
+    cr_assert_str_eq(sp[0], "");
+    cr_assert_str_eq(sp[1], "hello");
+    cr_assert_str_eq(sp[2], "world");
+    cr_assert_not(sp[3]);
+}
